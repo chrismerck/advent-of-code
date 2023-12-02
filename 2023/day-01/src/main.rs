9a030a2b6360a83b3c5897a2ff4e228f8a1c6dc1
Chris Merck <chrismerck@gmail.com>
2023-12-02 08:22:08 -0300

    day01: print arg

---
+++ 2023/day-01/src/main.rs
@@ -0,0 +1,18 @@
+// AoC 2023 -- Day 01
+
+// Read a file and sum the two-digit numbers
+// formed by combining the first and last digit
+// of each line, which may be the same character.
+
+fn main() {
+    // get first command line argument
+    println!("{:?}", std::env::args());
+    // open file
+    // for each line
+        // regex to get first and last digit
+        // assert has at least first digit
+        // if not last digit, use copy of first digit
+        // construct 2-digit number
+        // add to accumulator
+    // print result
+}
