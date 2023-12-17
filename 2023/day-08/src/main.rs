2c2b804c3e03b939da361ff8e6f1ead39f99c3cd
Chris Merck <chrismerck@gmail.com>
2023-12-17 15:17:01 -0500

    day-08: part 2 -- too slow

--- 2023/day-08/src/main.rs
+++ 2023/day-08/src/main.rs
@@ -12,7 +12,7 @@ GGG = (GGG, GGG)
 ZZZ = (ZZZ, ZZZ)
 */
 
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::fs::File;
 use std::io::{BufRead, BufReader};
 use std::env;
@@ -32,7 +32,11 @@ fn parse_input() -> (Vec<char>, HashMap<String, (String, String)>) {
     // parse the rest
     for line in lines {
         let line = line.unwrap();
-        let line = line.chars().filter(|c| c.is_alphabetic() || c.is_whitespace()).collect::<String>();
+        let line = line.chars().filter(|c| 
+               c.is_alphabetic() 
+            || c.is_whitespace()
+            || c.is_numeric()
+        ).collect::<String>();
         let line = line.replace("  ", " ");
         let mut line = line.split(" ").collect::<Vec<&str>>();
         let key = line.remove(0).to_string();
@@ -51,12 +55,11 @@ fn step(instr: char, map: &HashMap<String, (String, String)>, start: &String) ->
     }
 }
 
-fn main() {
-    let (instr, map) = parse_input();
+fn part1(instr: &Vec<char>, map: &HashMap<String, (String, String)>) {
     let mut pos = "AAA".to_string();
     let mut step_count = 0;
     loop {
-        for i in &instr {
+        for i in instr {
             step_count += 1;
             pos = step(*i, &map, &pos);
             if pos == "ZZZ" {
@@ -65,4 +68,37 @@ fn main() {
             }
         }
     }
+}
+
+fn part2(instr: &Vec<char>, map: &HashMap<String, (String, String)>) {
+    let mut positions : Vec<String> = 
+        map.keys().filter(|k| k.ends_with("A")).map(|k| k.clone()).collect();
+
+    let mut step_count = 0;
+    loop {
+        //println!("Positions: {:?}", positions);
+        let mut new_positions = Vec::new();
+        for i in instr {
+            step_count += 1;
+            for pos in &positions {
+                let new_pos = step(*i, &map, pos);
+                //println!("  {} -> {}", pos, new_pos);
+                new_positions.push(new_pos);
+            }
+            positions = new_positions.clone();
+            new_positions.clear();
+        }
+        if positions.iter().all(|p| p.ends_with("Z")) {
+            println!("Got to all ..Z in {} steps", step_count);
+            return;
+        }
+    }
+}
+
+fn main() {
+    let (instr, map) = parse_input();
+    println!("Instructions: {:?}", instr);
+    println!("Map: {:?}", map);
+    //part1(&instr, &map);
+    part2(&instr, &map);
 }
